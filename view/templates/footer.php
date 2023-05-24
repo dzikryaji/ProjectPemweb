3858f4c3eec8d1e29ea3311ae4d5c50297e26696
dzikryaji <dzikryaji07@gmail.com>
2023-05-24 17:36:46 +0700

    adding footer

--- view/templates/footer.php
+++ view/templates/footer.php
@@ -1,3 +1,35 @@
+    <hr class="mx-4">
+    <footer class="d-flex flex-row justify-content-around p-2">
+        <div>
+            <h6 class="mb-2">QUICK LINKS</h6>
+            <ul class="list-unstyled">
+                <li>Account</li>
+                <li>About Us </li>
+                <li>Contact Us</li>
+                <li>Delivery Info</li>
+                <li>Wishlist FAQ</li>
+                <li>FAQ</li>
+            </ul>
+        </div>
+        <div>
+            <h6 class="mb-2">CATEGORIES</h6>
+            <ul class="list-unstyled">
+                <li>Fruits</li>
+                <li>Vegetables</li>
+                <li>Berries</li>
+                <li>Nuts</li>
+                <li>Seeds</li>
+            </ul>
+        </div>
+        <div>
+            <h6 class="mb-2">CUSTOMER SERVICE</h6>
+            <ul class="list-unstyled">
+                <li class="mt-2">523 Sylvan Ave, 5th FloorMountain View, CA 94041 USA</li>
+                <li class="mt-2">+1-(844)-123-456-78</li>
+                <li class="mt-2">vegan@link.org</li>
+            </ul>
+        </div>
+    </footer>
     <script src="<?= BASEURL ?>/bootstrap/js/bootstrap.bundle.js"></script>
 </body>
 </html>
