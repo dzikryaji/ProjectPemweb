6b3cf508e5ad3cee521cc4ff74b2576d5bd1597b
dzikryaji <dzikryaji07@gmail.com>
2023-05-27 16:50:05 +0700

    add product table ddl

--- ddl.sql
+++ ddl.sql
@@ -8,3 +8,13 @@ CREATE TABLE `project_pemweb`.`user` (
         PRIMARY KEY (`id`), 
         UNIQUE (`email`)
 ) ENGINE = InnoDB;
+
+CREATE TABLE `project_pemweb`.`product`(
+    `id_product` INT NOT NULL AUTO_INCREMENT , 
+    `product_name` VARCHAR(255) NOT NULL , 
+    `price` INT NOT NULL , 
+    `stock` INT NOT NULL , 
+    `description` VARCHAR(255) NOT NULL , 
+    `product_image_name` VARCHAR(255) NOT NULL , 
+    PRIMARY KEY (`id_product`)
+) ENGINE = InnoDB;
