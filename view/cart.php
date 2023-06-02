f3e73c0a06f31684d7e6b13aa01fafdd222d75f6
marsaa <marsaafatin.h@gmail.com>
2023-06-02 21:20:43 +0700

    add cart feature for products

---
+++ view/cart.php
@@ -0,0 +1,73 @@
+<section class="container py-5">
+    <h1>Your Cart</h1>
+    <h5 class="mb-3">Not ready to Checkout? Continue Shopping</h5>
+    <div class="row mb-4">
+        <div class="col-md-8 pe-2 pe-lg-5">
+            <div class="row border-bottom border-secondary py-3 mb-3">
+                <div class="col-2 bg-light">
+                    <!-- <img src="/" alt=""> -->
+
+                </div>
+                <div class="col-10">
+                    <h4>Natural Honey</h4>
+                    <small>Quantity: 1</small>
+                    <h4>$10</h4>
+                    <div class="text-end">
+                        <a href="" class="btn text-dark text-decoration-underline">Remove</a>
+                    </div>
+                </div>
+            </div>
+            <div class="row py-3 mb-3">
+                <div class="col-2 bg-light">
+                    <!-- <img src="/" alt=""> -->
+
+                </div>
+                <div class="col-10">
+                    <h4>Natural Honey</h4>
+                    <small>Quantity: 1</small>
+                    <h4>$10</h4>
+                    <div class="text-end">
+                        <a href="" class="btn text-dark text-decoration-underline">Remove</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <h4 class="mb-4">Order Summary</h4>
+            <div class="clearfix mb-2">
+                <div class="float-start">
+                    <h5>Subtotal</h5>
+                </div>
+                <div class="float-end">
+                    <h5>$10</h5>
+                </div>
+            </div>
+            <div class="clearfix border-bottom border-secondary pb-3 mb-3">
+                <div class="float-start">
+                    <h5>Delivery</h5>
+                </div>
+                <div class="float-end">
+                    <small class="text-muted">Calculated at the next step</small>
+                </div>
+            </div>
+            <div class="clearfix pb-3">
+                <div class="float-start">
+                    <h5>Total</h5>
+                </div>
+                <div class="float-end">
+                    <h5>$10</h5>
+                </div>
+            </div>
+            <button class="btn bg-dark text-white d-block w-100 py-3">Continue to Checkout</button>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-8">
+            <h4 class="mb-4 pb-3 border-bottom border-secondary">Order Information</h4>
+            <span class="text-muted">
+                <h6>Return Policy</h6>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea dignissimos sit sequi iure obcaecati dolore autem tenetur aspernatur soluta debitis!</p>
+            </span>
+        </div>
+    </div>
+</section>
