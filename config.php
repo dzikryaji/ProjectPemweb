c52a33a85872037f5717cbc7292eb9b5615339ce
dzikryaji <dzikryaji07@gmail.com>
2023-06-06 22:45:30 +0700

    update config UPLOADDIR

--- config.php
+++ config.php
@@ -5,4 +5,4 @@ define("DBHOST", "127.0.0.1");
 define("DBUSERNAME", "root");
 define("DBPASSWORD", "");
 define("DBNAME", "project_pemweb");
-define("UPLOADDIR", "uploads");
+define("UPLOADDIR", "asset/uploads");
