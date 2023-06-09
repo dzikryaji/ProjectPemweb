d659e747b5f9a3e9823de24005a81837606d5162
marsaa <marsaafatin.h@gmail.com>
2023-06-09 19:27:07 +0700

    add checkout

--- index.php
+++ index.php
@@ -6,6 +6,7 @@ $method = $_GET['m'] ?? "index";
 session_start();
 
 include_once "controller/BaseController.php";
+include_once "controller/checkout.php";
 include_once "controller/$controller.php";
 include_once "config.php";
 include_once "Flasher.php";
