fbb4608d91d96fcf6f964d777bd5f2e0888572ab
marsaa <marsaafatin.h@gmail.com>
2023-06-09 19:33:13 +0700

    add query for payment and address

--- ddl.sql
+++ ddl.sql
@@ -27,3 +27,24 @@ CREATE TABLE `project_pemweb`.`cart`(
     FOREIGN KEY (`id_user`) REFERENCES user(`id`), 
     FOREIGN KEY (`id_product`) REFERENCES product(`id_product`) 
 ) ENGINE = InnoDB;
+
+CREATE TABLE `project_pemweb`.`address`( 
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `contact_number` varchar(255) NOT NULL,
+  `city` varchar(255) NOT NULL,
+  `province` varchar(255) NOT NULL,
+  `optional` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `project_pemweb`.`payment`( 
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `address_id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `number` varchar(255) NOT NULL,
+  `month` varchar(2) NOT NULL,
+  `year` varchar(4) NOT NULL,
+  `cvc` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
