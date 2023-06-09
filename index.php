aa317bf9e2b80cc0510b90adc5242b76e4adf818
marsaa <marsaafatin.h@gmail.com>
2023-06-09 20:58:30 +0700

    delete line 9 on index

--- index.php
+++ index.php
@@ -6,7 +6,6 @@ $method = $_GET['m'] ?? "index";
 session_start();
 
 include_once "controller/BaseController.php";
-include_once "controller/checkout.php";
 include_once "controller/$controller.php";
 include_once "config.php";
 include_once "Flasher.php";
