e8e3939ef58ea81069a5ee1c9dc141d5551922aa
marsaa <marsaafatin.h@gmail.com>
2023-06-09 20:01:57 +0700

    add get product and delete feature for cart

--- model/CartModel.php
+++ model/CartModel.php
@@ -28,4 +28,36 @@ class CartModel extends BaseModel
             echo $this->conn->error;
         }
     }
+
+    function get() {
+        $sql = "SELECT * FROM `cart` a
+        left join user b on b.id = a.id_user
+        left join product c on c.id_product = a.id_product;";
+        
+        $result = $this->conn->query($sql);
+        
+        $user = $result->fetch_all(MYSQLI_ASSOC);
+
+        return $user;
+    }
+
+    function delete($id_user, $id_product)
+    {
+
+        $sql = "DELETE FROM cart WHERE id_user = $id_user and id_product = $id_product";
+
+
+        try {
+            $stmt = $this->conn->stmt_init();
+            
+            $stmt->prepare($sql);
+
+            $stmt->execute();
+
+            header('Location: ' . BASEURL . 'c=cart&m=index');
+            exit;
+        } catch (Exception $e) {
+            echo $this->conn->error;
+        }
+    }
 }
